da7123d0ceda81482c0229258c2c16d36230eed2
AlecFaye <kelvintran.lim@outlook.com>
2024-01-25 05:24:28 -0500

    Fixed import names

--- rptodo/__main__.py
+++ rptodo/__main__.py
@@ -1,4 +1,4 @@
-from cli_todo import cli, __app_name__
+from rptodo import cli, __app_name__
 
 def main():
     cli.app(prog_name=__app_name__)
--- rptodo/cli.py
+++ rptodo/cli.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import typer
 
-from cli_todo import __app_name__, __version__
+from rptodo import __app_name__, __version__
 
 app = typer.Typer()
 
