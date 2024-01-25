3a6afc5b62d4de095fd62a77a81f052a6d811efc
AlecFaye <kelvintran.lim@outlook.com>
2024-01-25 06:00:49 -0500

    Get the To-Do database ready for use

---
+++ rptodo/database.py
@@ -0,0 +1,23 @@
+import configparser
+from pathlib import Path
+
+from rptodo import DB_WRITE_ERROR, SUCCESS
+
+DEFAULT_DB_FILE_PATH = Path.home().joinpath(
+    "." + Path.home().stem + "_todo.json"
+)
+
+"""Return the current path to the to-do database."""
+def get_database_path(config_file: Path) -> Path:
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    
+    return Path(config_parser["General"]["database"])
+
+"""Create the to-do database."""
+def init_database(db_path: Path) -> int:
+    try:
+        db_path.write_text("[]")  # Empty to-do list
+        return SUCCESS
+    except OSError:
+        return DB_WRITE_ERROR
